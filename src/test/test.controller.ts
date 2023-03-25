8f763182a9902429ee790defab6c1b665757f665
zeyujay <zeyujay@gmail.com>
2023-03-26 03:10:00 +0800

    去除type

--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TestService } from './test.service';
 import { CreateTestDto } from './dto/create-test.dto';
 import { Test } from './schemas/test.schema';
@@ -17,10 +25,10 @@ export class TestController {
     return this.testService.findAll();
   }
 
-  @Get(':id')
+  /* @Get(':id')
   async findOne(@Param('id') id: string): Promise<Test> {
     return this.testService.findOne(id);
-  }
+  } */
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
