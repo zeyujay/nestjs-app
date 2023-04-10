eb324e579a8195aa7a843ec91dec466fc96b204c
zeyujay <zeyujay@gmail.com>
2023-04-11 00:38:20 +0800

    123

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,18 @@
  * @Author: zeyujay zeyujay@gmail.com
  * @Date: 2023-03-11 00:58:20
  * @LastEditors: zeyujay zeyujay@gmail.com
- * @LastEditTime: 2023-03-30 04:48:33
- * @FilePath: /notion-book/Users/zeyu/Documents/work/nestjs-app/src/main.ts
+ * @LastEditTime: 2023-04-11 00:37:16
+ * @FilePath: /note/Users/zeyu/Documents/work/nestjs-app/src/main.ts
  * @Description:1111
  *
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
+//import * as fs from 'fs';
 async function bootstrap() {
   console.log(process.env.NODE_ENV);
-  const httpsOptions =
+  /* const httpsOptions =
     process.env.NODE_ENV === 'development'
       ? null
       : {
@@ -25,7 +25,8 @@ async function bootstrap() {
       ? await NestFactory.create(AppModule)
       : await NestFactory.create(AppModule, {
           httpsOptions,
-        });
+        }); */
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   await app.listen(3002);
